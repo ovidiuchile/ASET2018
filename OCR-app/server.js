af9b618440cab597f528244e5ed3f9ed1f39575f
Anda Costina UNGHIANU <aunghianu@bitdefender.com>
2019-01-09 11:07:00 +0200

    fix

--- OCR-app/server.js
+++ OCR-app/server.js
@@ -9,6 +9,7 @@ const uuidv1 = require('uuid/v1');
 const rnScriptPath = 'rn/nn_get_text.py';
 const { exec } = require('child_process');
 const fs = require('fs');
+const request = require('request');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -30,19 +31,20 @@ app.post('/upload', upload.single('image'), function(req, res) {
     //     res.send(icrResult);
     //     res.end();
     // });
+    let filePath = __dirname + '/uploads/' + filename;
+    let formData = {
+        'file':{
+            value: fs.readFileSync(filePath),
+            options:{
+                contentType: req.file.mimetype,
+                filename: req.file.originalname
+            }
+        }
+    };
     request({
       url: 'http://127.0.0.1:5000',
       method: 'POST',
-      formData: {
-        'regularField': 'someValue',
-        'regularFile': someFileStream,
-        'customBufferFile': {
-          value: fileBufferData,
-          options: {
-            filename: 'myfile.bin'
-          }
-        }
-      }
+      formData
     }, function(err, resp, body) {
         // res.send(body);
         console.log(body);
